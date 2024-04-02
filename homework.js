3ea5205833d9db63fcb7a1fac50be0ee7c75abc9
caleb swenson <calebswenson@calebs-MacBook-Pro.local>
2024-04-02 14:41:03 -0700

    add style

--- homework.js
+++ homework.js
@@ -4,15 +4,22 @@ console.log(welcomeMsg);
 
 const temperatureC = 25;
 const temperatureF = (temperatureC * 9/5) + 32;
-
-if (temperatureF > 80) {
-    console.log("It's a hot day!");
-} else {
-    console.log("It's not too hot today.");
+function task2(){
+    if (temperatureF > 80) {
+        console.log("It's a hot day!");
+        alert("It's a hot day!")
+    } else {
+        console.log("It's not too hot today.");
+        alert("It's not too hot today.")
+    }
 }
+
 
 let count = 0;
 while (count <= 50) {
     console.log(count);
     count += 5;
+}
+function task1() {
+    alert("Hello, " + name + "!");
 }
